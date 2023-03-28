e0ce1dd110c2233bdf16528abc7296f33e948538
Sajjad Aemmi <sajjadaemmi@gmail.com>
2023-03-29 00:23:35 +0330

    v0.1.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ pre_install()
 
 setup(
     name='pyvpd',
-    version='0.1.1',
+    version='0.1.2',
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=["pyvpd"],
